0f47be850faf92c6cfcddcf6e1f2884ba2abfc32
Takashi Miwa <sanrinsha.tm@gmail.com>
2011-12-18 11:14:01 +0900

    first comit

---
+++ link.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+ln -svn $HOME/git/dotfiles/.bash_profile $HOME
+ln -svn $HOME/git/dotfiles/.bashrc $HOME
+ln -svn $HOME/git/dotfiles/.inputrc $HOME
+ln -svn $HOME/git/dotfiles/.screenrc $HOME
+ln -svn $HOME/git/dotfiles/.vimrc $HOME
+ln -svn $HOME/git/dotfiles/.zashrc $HOME
+ln -svn $HOME/git/dotfiles/.zshrc $HOME
