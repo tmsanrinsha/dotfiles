7b498ec0834599928e82a2830eb0653133c39389
Takashi Miwa <sanrinsha.tm@gmail.com>
2011-12-23 17:36:55 +0900

    .zashrc: PATHの設定

--- link.sh
+++ link.sh
@@ -2,7 +2,12 @@
 ln -svn $HOME/git/dotfiles/.bash_profile $HOME
 ln -svn $HOME/git/dotfiles/.bashrc $HOME
 ln -svn $HOME/git/dotfiles/.inputrc $HOME
+ln -svn $HOME/git/dotfiles/.my.cnf $HOME
 ln -svn $HOME/git/dotfiles/.screenrc $HOME
 ln -svn $HOME/git/dotfiles/.vimrc $HOME
 ln -svn $HOME/git/dotfiles/.zashrc $HOME
 ln -svn $HOME/git/dotfiles/.zshrc $HOME
+mkdir $HOME/bin
+ln -svn $HOME/git/dotfiles/bin/apacheErrorColor.sed $HOME/bin
+ln -svn $HOME/git/dotfiles/bin/fxg $HOME/bin
+ln -svn $HOME/git/dotfiles/bin/tree $HOME/bin
