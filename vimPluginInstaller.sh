2293bf309f1fa647d93c7d2c546f15a9d42a09f4
Takashi Miwa <sanrinsha.tm@gmail.com>
2012-01-21 10:34:54 +0900

    minor change

--- vimPluginInstaller.sh
+++ vimPluginInstaller.sh
@@ -1,6 +1,12 @@
 #!/usr/local/bin/bash -x
-# vimのpluginをインストールする
-cd ~/git &&
-# vim-emacscommandline
-git clone git://github.com/houtsnip/vim-emacscommandline.git &&
-cp -r vim-emacscommandline/* ~/.vim
+## vimのpluginをインストールする
+# sudo.vimのインストール
+if [ ! -f ~/.vim/plugin/sudo.vim ]; then
+    cd ~/.vim/plugin || exit 1
+    curl -o sudo.vim "http://www.vim.org/scripts/download_script.php?src_id=3477"
+fi
+
+#cd ~/git &&
+## vim-emacscommandline
+#git clone git://github.com/houtsnip/vim-emacscommandline.git &&
+#cp -r vim-emacscommandline/* ~/.vim
