b140b98f15a4b9266d2dfb6885f51a3d473a16a7
Takashi Miwa <sanrinsha.tm@gmail.com>
2012-02-12 23:19:50 +0900

    fxg.sh: 第一変数をfindの引数、第二引数をegrepの引数に設定した

--- bin/fxg.sh
+++ bin/fxg.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!//bin/sh
 # find $1 -type f -print0 | xargs -0 grep --color=auto $2を
 # するシェルスクリプト
 # 引数が1つの場合は、カレントディレクトリを$1で検索
@@ -8,9 +8,15 @@ usage() {
     echo "${COMMAND} [<dir>] <search name>"
     exit
 }
-[ "$1" = '-h' -o "$1" = '-help' ! ( $# -eq 1 -o $# -eq 2) ] && usage
+[ "$1" = '-h' -o "$1" = '-help' -o ! \( $# -eq 1 -o $# -eq 2 \) ] && usage
 
-[ "$#" eq 1 ] && ARG1=$1 && $1=1
-#find "${DIR}" -type f -print0 | xargs -0 grep --color=auto $1
+if [ "$#" -eq 1 ];then
+    ARG1=.
+    ARG2=$1
+else
+    ARG1=$1
+    ARG2=$2
+fi
+
 #ドットで始まるディレクトリは検索しない
-find "${DIR}" -type f ! -regex ".*/\..*/.*" -print0 | xargs -0 grep --color=auto $1
+find $ARG1 -type f ! -regex ".*/\..*/.*" -print0 | xargs -0 egrep --color=auto $ARG2
