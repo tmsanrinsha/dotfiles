736abc06979f58f1ffaa9fc69a0b9bf1b9da47dc
Takashi Miwa <sanrinsha.tm@gmail.com>
2012-03-03 11:40:24 +0900

    link.sh: Add .zashrc.local

--- link.sh
+++ link.sh
@@ -8,6 +8,7 @@ ln -svn $HOME/git/my_git/dotfiles/.screenrc $HOME
 ln -svn $HOME/git/my_git/dotfiles/.tmux.conf $HOME
 ln -svn $HOME/git/my_git/dotfiles/.vimrc $HOME
 ln -svn $HOME/git/my_git/dotfiles/.zashrc $HOME
+ln -svn $HOME/git/my_git/dotfiles/.zashrc.local $HOME
 ln -svn $HOME/git/my_git/dotfiles/.zshrc $HOME
 mkdir $HOME/bin
 ln -svn $HOME/git/my_git/dotfiles/bin/apacheErrorColor.sed $HOME/bin
