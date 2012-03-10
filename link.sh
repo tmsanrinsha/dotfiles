65a10fc02db244cd0c391e1210d9fa50bc17eb3f
Takashi Miwa <sanrinsha.tm@gmail.com>
2012-03-10 12:54:14 +0900

    lish.sh: Add decomp.sh

--- link.sh
+++ link.sh
@@ -10,7 +10,7 @@ ln -svn $HOME/git/my_git/dotfiles/.vimrc $HOME
 ln -svn $HOME/git/my_git/dotfiles/.zashrc $HOME
 ln -svn $HOME/git/my_git/dotfiles/.zashrc.local $HOME
 ln -svn $HOME/git/my_git/dotfiles/.zshrc $HOME
-mkdir $HOME/bin
+mkdir $HOME/script
 ln -svn $HOME/git/my_git/dotfiles/script/apacheErrorColor.sed $HOME/script
 ln -svn $HOME/git/my_git/dotfiles/script/decomp.sh $HOME/script
 ln -svn $HOME/git/my_git/dotfiles/script/fxg.sh $HOME/script
