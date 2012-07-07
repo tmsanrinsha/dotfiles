b64a893a0e4ca98debabb288249a3ab2bf5f96e4
Takashi Miwa <sanrinsha.tm@gmail.com>
2012-07-07 22:57:05 +0900

    setup.shで.gitにもシンボリックリンクを貼ってしまうのを改善

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-PWD=`pwd`
-ln -svn $PWD/.* $HOME
+# dotfilesにシンボリックリンクを貼る
+find `pwd` -type f -regex ".*/\..*" ! -regex ".*/\.git.*" | xargs -I{} ln -svn {} $HOME
+
 mkdir $HOME/bin
 # 実行ファイル系はハードリンクにする。
 # $HOME/binにパスが通っているとする
+PWD=`pwd`
 ln -vn $PWD/bin/* $HOME/bin
 
 # http://betterthangrep.com/
-curl http://betterthangrep.com/ack-standalone > ~/local/bin/ack && chmod 0755 !#:3
+curl http://betterthangrep.com/ack-standalone > ~/local/bin/ack && chmod 0755 ~/local/bin/ack
