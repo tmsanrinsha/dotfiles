3bedddf260fabcb6bbd3287e23be826016a1c41c
Takashi Miwa <sanrinsha.tm@gmail.com>
2012-09-22 12:40:35 +0900

    cygwin対応

--- setup.sh
+++ setup.sh
@@ -1,17 +1,30 @@
 #!/bin/sh
-set -x
-# dotfilesにシンボリックリンクを貼る
-find `pwd` -type f -regex ".*/\..*" ! -regex ".*/\.git.*" | xargs -I{} ln -svn {} $HOME
 
-mkdir $HOME/bin
+if [[ `uname` = CYGWIN* ]]; then
+    [ ! -d ~/bin/cygwin ] && mkdir -p ~/bin/cygwin
+    if [ ! -x ~/bin/cygwin/ln ]; then
+        curl -L https://raw.github.com/tmsanrinsha/dotfiles/master/bin/cygwin/ln > ~/bin/cygwin/ln
+        chmod 0755 ~/bin/cygwin/ln
+        ln=~/bin/cygwin/ln
+    fi
+else
+    ln=`which ln`
+fi
+
+[ ! -d ~/bin ] && mkdir ~/bin
 # 実行ファイル系はハードリンクにする。
 # $HOME/binにパスが通っているとする
-PWD=`pwd`
-ln -vn $PWD/bin/* $HOME/bin
+find `pwd`/bin -maxdepth 1 -type f | xargs -I{} $ln -v {} $HOME/bin
+#$ln -v $PWD/bin/* $HOME/bin
+
+# dotfilesにシンボリックリンクを貼る
+find `pwd` -type f -regex ".*/\..*" ! -regex ".*/\.git.*" | xargs -I{} $ln -sv {} $HOME
+
 
 # http://betterthangrep.com/
 if [ ! -f ~/bin/ack ];then
-    curl http://betterthangrep.com/ack-standalone > ~/bin/ack && chmod 0755 ~/bin/ack
+    curl http://betterthangrep.com/ack-standalone > ~/bin/ack
+    chmod a+x ~/bin/ack
 fi
 
 [ ! -d ~/bin/pseudo ] && mkdir ~/bin/pseudo
@@ -19,6 +32,8 @@ fi
 # http://d.hatena.ne.jp/hnw/20120602
 # https://github.com/hnw/fakegit
 if [ ! -f ~/bin/pseudo/git ];then
-    curl -L https://raw.github.com/hnw/fakegit/master/bin/fakegit > $HOME/bin/pseudo/git
+    curl -L https://raw.github.com/hnw/fakegit/master/bin/fakegit > ~/bin/pseudo/git
     chmod a+x $HOME/bin/pseudo/git
 fi
+
+. ~/.bash_profile
