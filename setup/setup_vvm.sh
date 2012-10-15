117bd72dc1bfd1fb85ee212dc0a0d38721891717
Takashi Miwa <sanrinsha.tm@gmail.com>
2012-10-16 08:55:28 +0900

    first commit

---
+++ setup/setup_vvm.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# http://labs.timedia.co.jp/2011/09/vim-version-manager.html
+# VVMを使って、7.3に最新のパッチを当てたvimをインストールする
+set -x
+
+if [ ! -f ~/.vvm/etc/login ]; then
+    curl https://raw.github.com/kana/vim-version-manager/master/bin/vvm | python - setup
+fi
+
+source ~/.vvm/etc/login
+
+vimorg_ver=`curl ftp://ftp.vim.org/pub/vim/patches/7.3/README | tail -1 | awk '{print $2}' | sed 's/\./-/g'`
+
+cd ~
+vvm install vimorg--v${vimorg_ver} --with-features=big --enable-multibyte --enable-pythoninterp --disable-gui --without-x
+vvm use vimorg--v${vimorg_ver}
