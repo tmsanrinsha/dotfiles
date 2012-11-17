b53d46a59bc16cf9df940cf285f5a8d81416fe69
Takashi Miwa <sanrinsha.tm@gmail.com>
2012-11-18 02:35:37 +0900

    Bug fix

--- setup/cpanm.sh
+++ setup/cpanm.sh
@@ -9,4 +9,4 @@ export PERL5LIB="${HOME}/perl5/lib/perl5:$PERL5LIB"
 cpanm --local-lib=~/perl5 local::lib
 test -d ~/.profile.d || mkdir ~/.profile.d
 echo 'export PERL5LIB="${HOME}/perl5/lib/perl5:$PERL5LIB"' > ~/.profile.d/cpanm.sh
-echo 'eval $(perl -I ~/perl6/lib/perl5/ -Mlocal::lib)'    >> ~/.profile.d/cpanm.sh
+echo 'eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)'    >> ~/.profile.d/cpanm.sh
