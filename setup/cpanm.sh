b1d9e7f72b89cb4b81e749b5049e3203823a1dc4
**** <****>
2013-01-29 00:01:18 +0900

    setup.shでcygwinのときにopensshとssh-pageant, ackをインストールするようにした

--- setup/cpanm.sh
+++ setup/cpanm.sh
@@ -10,3 +10,4 @@ cpanm --local-lib=~/perl5 local::lib
 test -d ~/.profile.d || mkdir ~/.profile.d
 echo 'export PERL5LIB="${HOME}/perl5/lib/perl5:$PERL5LIB"' > ~/.profile.d/cpanm.sh
 echo 'eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)'    >> ~/.profile.d/cpanm.sh
+source ~/.profile.d/cpanm.sh
--- setup/setup.sh
+++ setup/setup.sh
@@ -2,25 +2,44 @@
 
 set -ex
 
+gitdir=`pwd | sed 's|/setup$||'`
+
 if [[ `uname` = CYGWIN* ]]; then
+    # Windowsのメッセージの文字コードをcp932からutf-8に変更
+    # なぜか英語になっちゃう
+    cmd /c chcp 65001
     [ ! -d ~/bin/cygwin ] && mkdir -p ~/bin/cygwin
+
+    # cygwinのlnをmklinkで実行する
     if [ ! -x ~/bin/cygwin/ln ]; then
         curl -L https://raw.github.com/tmsanrinsha/dotfiles/master/bin/cygwin/ln > ~/bin/cygwin/ln
         chmod a+x ~/bin/cygwin/ln
         alias ln="~/bin/cygwin/ln"
     fi
+
     if [ ! -x ~/bin/cygwin/apt-cyg ]; then
-        curl -L http://apt-cyg.googlecode.com/svn/trunk/apt-cyg > ~/bin/cygwin/apt-cyg
+        curl -kL http://apt-cyg.googlecode.com/svn/trunk/apt-cyg > ~/bin/cygwin/apt-cyg
         chmod a+x ~/bin/cygwin/apt-cyg
     fi
-    # cmdのコマンドを打った時の文字コードをcp932からutf-8に変更
-    # なぜか英語になっちゃう
-    cmd /c chcp 65001
+
+    if which ssh 1>/dev/null 2>&1; then
+        apt-cyg install openssh
+    fi
+
+    # cygwinでpageantを使う
+    # http://sanrinsha.lolipop.jp/blog/2012/08/cygwin%E3%81%A7pageant%E3%82%92%E4%BD%BF%E3%81%86.html
+    if [ ! -x /usr/bin/ssh-pageant ]; then
+        cd /usr/src
+        curl -L https://github.com/downloads/cuviper/ssh-pageant/ssh-pageant-1.1-release.tar.gz | tar  zxvf -
+        cd ssh-pageant-1.1
+        cp ssh-pageant.exe /usr/bin/
+        cp ssh-pageant.1 /usr/share/man/man1/
+    fi
 fi
 
+cd $gitdir
 # リンクの作成
-gitdir=`pwd | sed 's|/setup$||'`
-for file in `find .. -maxdepth 1 -type f ! -regex '.*README.*' ! -regex '.*.git.*' | sed 's|../||'`
+for file in `find . -maxdepth 1 -type f ! -regex '.*README.*' ! -regex '.*.git.*' | sed 's|./||'`
 do
     [ ! -f ~/$file ] && ln -sv $gitdir/$file ~/$file
 done
@@ -28,10 +47,10 @@ done
 [ ! -d ~/bin ] && ln -sv $gitdir/bin ~/bin
 
 # http://betterthangrep.com/
-if [ ! -x ~/bin/ack ];then
-    curl http://betterthangrep.com/ack-standalone > ~/bin/ack
-    chmod a+x ~/bin/ack
-fi
+# if [ ! -x ~/bin/ack ];then
+#     curl http://betterthangrep.com/ack-standalone > ~/bin/ack
+#     chmod a+x ~/bin/ack
+# fi
 
 [ ! -d ~/bin/pseudo ] && mkdir ~/bin/pseudo
 
@@ -53,3 +72,4 @@ if ! which cpanm 1>/dev/null 2>&1;then
     source cpanm.sh
 fi
 cpanm --skip-installed MIME::Base64
+cpanm --skip-installed App::Ack
