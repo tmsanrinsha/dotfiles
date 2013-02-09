cec3693a9c2b5addef969ac117c4d6cfe5dbc58d
Takashi Miwa <sanrinsha.tm@gmail.com>
2013-02-10 03:04:17 +0900

    minor change

--- setup/setup.sh
+++ setup/setup.sh
@@ -4,19 +4,51 @@ set -ex
 
 gitdir=`pwd | sed 's|/setup$||'`
 
+[ ! -d ~/bin/pseudo ] && mkdir -p ~/bin/pseudo
+[ ! -d ~/bin/cygwin ] && mkdir -p ~/bin/cygwin
+
 if [[ `uname` = CYGWIN* ]]; then
     # Windowsのメッセージの文字コードをcp932からutf-8に変更
     # なぜか英語になっちゃう
     cmd /c chcp 65001
-    [ ! -d ~/bin/cygwin ] && mkdir -p ~/bin/cygwin
 
     # cygwinのlnをmklinkで実行する
     if [ ! -x ~/bin/cygwin/ln ]; then
-        curl -L https://raw.github.com/tmsanrinsha/dotfiles/master/bin/cygwin/ln > ~/bin/cygwin/ln
-        chmod a+x ~/bin/cygwin/ln
+        # curl -L https://raw.github.com/tmsanrinsha/dotfiles/master/bin/cygwin/ln > ~/bin/cygwin/ln
+        # chmod a+x ~/bin/cygwin/ln
+        cp $gitdir/bin/cygwin/ln ~/bin/cygwin/ln
         alias ln="~/bin/cygwin/ln"
     fi
+fi
 
+# リンクの作成とダウンロード
+for file in `find ..  -type f ! -regex '.*README.*' ! -regex '.*\.git.*' ! -regex '.*swp.*' ! -regex '.*setup.*' ! -regex '.*\.ssh.*' | sed 's|../||'`
+do
+    [ ! -f ~/$file ] && ln -sv $gitdir/$file ~/$file
+done
+[ ! -f ~/.gitconfig ] && cp $gitdir/.gitconfig ~/.gitconfig
+
+# http://betterthangrep.com/
+# if [ ! -x ~/bin/ack ];then
+#     curl http://betterthangrep.com/ack-standalone > ~/bin/ack
+#     chmod a+x ~/bin/ack
+# fi
+
+if [ ! -x ~/bin/pseudo/git ];then
+    # http://d.hatena.ne.jp/hnw/20120602
+    # https://github.com/hnw/fakegit
+    curl -L https://raw.github.com/hnw/fakegit/master/bin/fakegit > ~/bin/pseudo/git
+    chmod a+x $HOME/bin/pseudo/git
+fi
+
+# bin
+# [ ! -d ~/bin ] && mkdir ~/bin
+# for file in `find ../bin -maxdepth 1 -type f ! -regex '.*swp.*' | sed 's|../||'`
+# do
+#     [ ! -f ~/$file ] && ln -sv $gitdir/$file ~/$file
+# done
+
+if [[ `uname` = CYGWIN* ]]; then
     if [ ! -x ~/bin/cygwin/apt-cyg ]; then
         curl -kL http://apt-cyg.googlecode.com/svn/trunk/apt-cyg > ~/bin/cygwin/apt-cyg
         chmod a+x ~/bin/cygwin/apt-cyg
@@ -35,29 +67,6 @@ if [[ `uname` = CYGWIN* ]]; then
         cp ssh-pageant.exe /usr/bin/
         cp ssh-pageant.1 /usr/share/man/man1/
     fi
-fi
-
-# リンクの作成
-for file in `find .. -maxdepth 1 -type f ! -regex '.*README.*' ! -regex '.*.git.*' | sed 's|../||'`
-do
-    [ ! -f ~/$file ] && ln -sv $gitdir/$file ~/$file
-done
-[ ! -f ~/.gitconfig ] && cp $gitdir/.gitconfig ~/.gitconfig
-[ ! -d ~/bin ] && ln -sv $gitdir/bin ~/bin
-
-# http://betterthangrep.com/
-# if [ ! -x ~/bin/ack ];then
-#     curl http://betterthangrep.com/ack-standalone > ~/bin/ack
-#     chmod a+x ~/bin/ack
-# fi
-
-[ ! -d ~/bin/pseudo ] && mkdir ~/bin/pseudo
-
-# http://d.hatena.ne.jp/hnw/20120602
-# https://github.com/hnw/fakegit
-if [ ! -x ~/bin/pseudo/git ];then
-    curl -L https://raw.github.com/hnw/fakegit/master/bin/fakegit > ~/bin/pseudo/git
-    chmod a+x $HOME/bin/pseudo/git
 fi
 
 # vim
