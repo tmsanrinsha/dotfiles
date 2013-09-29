95187d67ada460ac49974cc9af5d7bb71ef3f604
Takashi Miwa <sanrinsha.tm@gmail.com>
2013-09-29 13:29:01 +0900

    minor change

--- setup/installer.sh
+++ setup/installer.sh
@@ -4,4 +4,4 @@ set -ex
 if [ -d ~/git/tmsanrinsha ]; then
     git clone git@github.com:tmsanrinsha/dotfiles.git ~/git/tmsanrinsha/dotfiles
 fi
-~/git/tmsanrinsha/dotfiles/script/common/setup/setup.sh
+~/git/tmsanrinsha/dotfiles/setup/setup.sh
