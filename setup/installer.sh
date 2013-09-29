76091e1255fd58532c8b3db4975415573f7d4f08
Takashi Miwa <sanrinsha.tm@gmail.com>
2013-09-29 13:28:11 +0900

    minor change

--- setup/installer.sh
+++ setup/installer.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
-set -xe
-git clone git@github.com:tmsanrinsha/dotfiles.git ~/git/tmsanrinsha/dotfiles
+set -ex
+
+if [ -d ~/git/tmsanrinsha ]; then
+    git clone git@github.com:tmsanrinsha/dotfiles.git ~/git/tmsanrinsha/dotfiles
+fi
 ~/git/tmsanrinsha/dotfiles/script/common/setup/setup.sh
