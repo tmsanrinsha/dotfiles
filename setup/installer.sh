f2a77b101630c4c1407032c292e0a4f499c07bb7
Takashi Miwa <sanrinsha.tm@gmail.com>
2013-10-26 17:20:32 +0900

    dotfilesのgit cloneをgitプロトコルに変更した

--- setup/installer.sh
+++ setup/installer.sh
@@ -3,6 +3,6 @@ set -ex
 
 # curl -L https://raw.github.com/tmsanrinsha/dotfiles/master/setup/installer.sh | bash
 if [ ! -d ~/git/tmsanrinsha ]; then
-    git clone git@github.com:tmsanrinsha/dotfiles.git ~/git/tmsanrinsha/dotfiles
+    git clone git://github.com/tmsanrinsha/dotfiles.git ~/git/tmsanrinsha/dotfiles
 fi
 ~/git/tmsanrinsha/dotfiles/setup/setup.sh
