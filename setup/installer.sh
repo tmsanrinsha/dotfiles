1b3b2ec7f5f7de3cb1a960a4cc6b28703d5eeb93
Takashi Miwa <sanrinsha.tm@gmail.com>
2014-01-12 01:24:46 +0900

    Revert "homeshickの修正"
    This reverts commit 27f041a538149944a7a03fa68e708d6cea5b1ef4.

--- setup/installer.sh
+++ setup/installer.sh
@@ -3,15 +3,13 @@ set -ex
 
 # curl -L https://raw.github.com/tmsanrinsha/dotfiles/master/setup/installer.sh | bash
 
-if ! test -d $HOME/.homesick/repos; then
-    mkdir -p $HOME/.homesick/repos
-    cd $HOME/.homesick/repos
-    git clone git://github.com/andsens/homeshick.git
+if ! test -d $HOME/.homesick/repos/homeshick; then
+    git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 fi
 source $HOME/.homesick/repos/homeshick/homeshick.sh
 
 if ! test -d $HOME/.homesick/repos/dotfiles; then
-    homeshick clone git://github.com/tmsanrinsha/dotfiles
+    homeshick clone tmsanrinsha/dotfiles
 fi
 
 git_dir=$HOME/.homesick/repos/dotfiles
