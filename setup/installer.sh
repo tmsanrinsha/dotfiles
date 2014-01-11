ce372b70380775f4a7891a29a390dfb2de80055f
Takashi Miwa <sanrinsha.tm@gmail.com>
2014-01-11 22:34:12 +0900

    bug fix

--- setup/installer.sh
+++ setup/installer.sh
@@ -3,12 +3,12 @@ set -ex
 
 # curl -L https://raw.github.com/tmsanrinsha/dotfiles/master/setup/installer.sh | bash
 
-if test -d $HOME/.homesick/repos/homeshick; then
+if ! test -d $HOME/.homesick/repos/homeshick; then
     git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 fi
 source $HOME/.homesick/repos/homeshick/homeshick.sh
 
-if test -d $HOME/.homesick/repos/dotfiles; then
+if ! test -d $HOME/.homesick/repos/dotfiles; then
     homeshick clone tmsanrinsha/dotfiles
 fi
 
