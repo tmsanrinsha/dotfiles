74f86bbef83ba0b675041dca67339347ee7f9daa
tmsanrinsha <sanrinsha.tm@gmail.com>
2014-02-11 16:40:33 +0900

    installer.shでディレクトリがないときは作成するようにした

--- setup/installer.sh
+++ setup/installer.sh
@@ -3,6 +3,7 @@ set -ex
 
 # curl -kL https://raw.github.com/tmsanrinsha/dotfiles/master/setup/installer.sh | bash
 if [ ! -d ~/git/tmsanrinsha ]; then
+    mkdir -p ~/git/tmsanrinsha
     git clone git://github.com/tmsanrinsha/dotfiles.git ~/git/tmsanrinsha/dotfiles
 fi
 bash ~/git/tmsanrinsha/dotfiles/setup/setup.sh
