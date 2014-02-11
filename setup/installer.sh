384e0c480a7ad617224426aff8f8f0853ea3560d
tmsanrinsha <sanrinsha.tm@gmail.com>
2014-02-11 16:47:45 +0900

    msysgitではディレクトリを指定したgit cloneうまく行かなかったのでcdすることにした

--- setup/installer.sh
+++ setup/installer.sh
@@ -4,6 +4,7 @@ set -ex
 # curl -kL https://raw.github.com/tmsanrinsha/dotfiles/master/setup/installer.sh | bash
 if [ ! -d ~/git/tmsanrinsha ]; then
     mkdir -p ~/git/tmsanrinsha
-    git clone git://github.com/tmsanrinsha/dotfiles.git ~/git/tmsanrinsha/dotfiles
+    cd ~/git/tmsanrinsha
+    git clone git://github.com/tmsanrinsha/dotfiles.git
 fi
 bash ~/git/tmsanrinsha/dotfiles/setup/setup.sh
