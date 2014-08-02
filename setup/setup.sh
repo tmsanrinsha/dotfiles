eb5e476065d901acbaa9e41c522d8f297b3d9b27
tmsanrinsha <sanrinsha.tm@gmail.com>
2014-08-03 00:05:33 +0900

    goの設定

--- setup/setup.sh
+++ setup/setup.sh
@@ -179,6 +179,7 @@ elif [[ `uname` = Darwin ]]; then
         fi
 
         brew install ant
+        brew install go
         brew install mercurial
         brew install nkf
         brew install node
