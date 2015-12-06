c91d4ccbe9bf2cd26c971802513922d986d53eb9
tmsanrinsha <sanrinsha.tm@gmail.com>
2015-12-07 00:51:30 +0900

    php.shにsql-formatterの通貨

--- setup/php.sh
+++ setup/php.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-curl -sS https://getcomposer.org/installer | php -d detect_unicode=Off -- --install-dir=$HOME/bin --filename=composer
-composer global require fabpot/php-cs-fixer
+which composer || curl -sS https://getcomposer.org/installer | php -d detect_unicode=Off -- --install-dir=$HOME/bin --filename=composer
+which php-cs-fixer  || composer global require fabpot/php-cs-fixer
+which sql-formatter || composer global require jdorn/sql-formatter:dev-master
