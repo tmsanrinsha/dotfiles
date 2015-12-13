de46b0deea9b744eb6adef6e58430630072fdc2a
tmsanrinsha <sanrinsha.tm@gmail.com>
2015-12-13 19:21:30 +0900

    phpunit.xmlの検出方法を修正

--- home/bin/phpunit.sh
+++ home/bin/phpunit.sh
@@ -1,40 +1,35 @@
 #!/usr/bin/env bash
 
+# original
 # composer で入れた phpunit で少し楽をする - Qiita
 # http://qiita.com/ngyuki/items/57ab6bb0e69590d5752d
 
 function func_phpunit
 {
-    local root="${PWD}"
+    local cmd="phpunit"
+    local dir="${PWD}"
 
-    while [ -n "${root}" ]; do
-        if [ -x "${root}/vendor/bin/phpunit" ]; then
+    while [ -n "${dir}" ]; do
+        if [ -x "${dir}/vendor/bin/phpunit" ]; then
+            cmd="${dir}/vendor/bin/phpunit"
             break
         fi
-        root="${root%/*}"
+        dir="${dir%/*}"
     done
 
-    if [ -z "${root}" ]; then
-        command phpunit "$@"
-        return $?
-    fi
-
-    local cmd=("${root}/vendor/bin/phpunit")
-
-    local dir xml
-
-    for dir in "/tests/" "/" ; do
-        for xml in "phpunit.xml.dist" "phpunit.xml"; do
-            if [ -e "${root}${dir}${xml}" ]; then
-                cmd=("${cmd[@]}" "--configuration=${root}${dir}")
-                break 2
-            fi
-        done
+    dir="${PWD}"
+    while [ -n "${dir}" ]; do
+        if [ -f "${dir}/phpunit.xml" ]; then
+            cmd="${cmd} --configuration=${dir}/phpunit.xml"
+            break
+        elif [ -f "${dir}/phpunit.xml.dist" ]; then
+            cmd="${cmd} --configuration=${dir}/phpunit.xml.dist"
+            break
+        fi
+        dir="${dir%/*}"
     done
 
-    cmd=("${cmd[@]}")
-
-    "${cmd[@]}" "$@" | cat
+    $cmd $@ | cat
     return ${PIPESTATUS[0]}
 }
 
