e71b6b21217ecff8f8e406d8d9e85124b12bdc10
tmsanrinsha <sanrinsha.tm@gmail.com>
2016-01-20 00:56:44 +0900

    php-cs-fixerの設定の追加

---
+++ home/bin/php-cs-fixer.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+function func_php-cs-fixer
+{
+    local cmd=$(which php-cs-fixer 2>/dev/null)
+
+    if [ -z "$cmd" ]; then
+        echo 'command not found: php-cs-fixer'
+        return 1
+    fi
+
+    local dir="${PWD}"
+
+    while [ -n "${dir}" ]; do
+        if [ -f "${dir}/.php_cs" ]; then
+            cmd="${cmd} --config-file=${dir}/.php_cs"
+            break 1
+        fi
+        dir="${dir%/*}"
+    done
+
+    php -d open_basedir= $cmd fix --diff $@
+}
+
+func_php-cs-fixer $@
