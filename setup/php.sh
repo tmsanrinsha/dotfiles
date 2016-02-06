5e063f43b1f61a9de92dd6327297c7452c0e506f
tmsanrinsha <sanrinsha.tm@gmail.com>
2016-02-06 21:25:12 +0900

    composer global installでインストールするように修正

--- setup/php.sh
+++ setup/php.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 which composer || curl -sS https://getcomposer.org/installer | php -d detect_unicode=Off -- --install-dir=$HOME/bin --filename=composer
-which php-cs-fixer  || composer global require fabpot/php-cs-fixer
-which sql-formatter || composer global require jdorn/sql-formatter:dev-master
+composer global install
