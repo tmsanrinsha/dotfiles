557d067f90eba24c0ae779daef46bbf635171de5
tmsanrinsha <sanrinsha.tm@gmail.com>
2016-03-13 12:57:47 +0900

    cronの設定の修正

---
+++ setup/setup_local.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -ex
+
+
+cd "$(dirname "${BASH_SOURCE:-$0}")"
+./setup.sh
+
+if [[ $(echo "$(vim --version | head -n1 | cut -d' ' -f5) >= 7.4" | bc) -eq 1 ]];then
+    vim -N -u $HOME/.vimrc -c "try | call dein#update() | finally | qall! | endtry" -U NONE -i NONE -V1 -e -s || :
+fi
+
+crontab crontab.conf
