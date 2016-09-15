afd676439647158eb582596db02599dfbef739a1
tmsanrinsha <sanrinsha.tm@gmail.com>
2016-09-16 01:21:07 +0900

    vimのupdateの仕方

--- setup/setup_local.sh
+++ setup/setup_local.sh
@@ -7,8 +7,9 @@ source setup.sh
 
 cd "$(dirname "${BASH_SOURCE:-$0}")"
 
-if [[ $(echo "$(vim --version | head -n1 | cut -d' ' -f5) >= 7.4" | bc) -eq 1 ]];then
-    vim -N -u $HOME/.vimrc -c "try | call dein#update() | finally | qall! | endtry" -U NONE -i NONE -V1 -e -s || :
+vim=/Applications/MacVim.app/Contents/MacOS/Vim
+if [[ $(echo "$($vim --version | head -n1 | cut -d' ' -f5) >= 7.4" | bc) -eq 1 ]];then
+    $vim -N -u $HOME/.vimrc -c "try | call dein#update() | finally | qall! | endtry" -U NONE -i NONE -V1 -e -s || :
 fi
 
 crontab crontab.conf
