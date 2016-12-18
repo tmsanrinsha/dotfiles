3cf08a29dcfd366af28e2f89c6cf8e058e459cfc
tmsanrinsha <sanrinsha.tm@gmail.com>
2016-12-19 00:01:18 +0900

    vimのpluginのinstallスクリプトにyesを追加

--- home/bin/vim_plugin_install.sh
+++ home/bin/vim_plugin_install.sh
@@ -7,5 +7,5 @@ if [ -x /Applications/MacVim.app/Contents/MacOS/Vim ]; then
 fi
 
 if [[ $(echo "$($vim --version | head -n1 | cut -d' ' -f5) >= 7.4" | bc) -eq 1 ]];then
-    $vim -N -u $HOME/.vimrc -c "try | call dein#update() | finally | qall! | endtry" -U NONE -i NONE -V1 -e -s || :
+    yes | $vim -N -u $HOME/.vimrc -c "try | call dein#update() | finally | qall! | endtry" -U NONE -i NONE -V1 -e -s || :
 fi
