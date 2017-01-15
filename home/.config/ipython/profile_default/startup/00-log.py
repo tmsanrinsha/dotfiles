9fa49d1bfae01b27796a56228e0d59a435c50df6
tmsanrinsha <sanrinsha.tm@gmail.com>
2017-01-16 00:21:39 +0900

    ipythonでログを記録する

---
+++ home/.config/ipython/profile_default/startup/00-log.py
@@ -0,0 +1,17 @@
+# startup以下のファイルは辞書順で読み込まれる
+# - [IPython起動時にスクリプトを自動的に読み込む - Qiita](http://qiita.com/yuku_t/items/e1b30b85e09cf55c7486)
+
+# [iPythonでコマンド履歴ログ出力を自動化する - ほんじゃら堂](http://blog.honjala.net/entry/2016/06/06/234708)
+from time import strftime
+import os
+
+log_dir = os.path.expanduser('~') + '/.cache/ipython/' + strftime('%Y/%m')
+
+if not os.path.isdir(log_dir):
+    os.makedirs(log_dir)
+
+log_file = log_dir + strftime('/%Y-%m-%d-%H-%M-%S') + ".py"
+get_ipython().run_line_magic('logstart', '-o -t %s append' % log_file)
+
+del log_dir
+del log_file
