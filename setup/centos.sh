59e5e4f7dabfb276250d3f845e0c9cd75e5323db
tmsanrinsha <sanrinsha.tm@gmail.com>
2017-07-16 12:48:43 +0900

    centos.shの修正

--- setup/centos.sh
+++ setup/centos.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
-
+set -e
 sudo yum -y install ctags
 sudo yum -y install git
+
+# pyenv
+sudo yum -y install bzip2-devel openssl-devel readline-devel sqlite-devel zlib-devel
 
 # for vim
 sudo yum -y install gcc make ncurses-devel
@@ -12,21 +15,38 @@ sudo yum -y install lua-devel
 
 git clone --depth 1 https://github.com/vim/vim.git ~/src/github.com/vim/vim
 cd ~/src/github.com/vim/vim/src
+# make distclean || :
+# rm auto/config.cache || :
 
-./configure \
---with-features=huge \
---enable-multibyte \
---disable-gui \
---without-x \
---enable-rubyinterp \
---enable-pythoninterp \
---enable-perlinterp \
---enable-luainterp
+# pyenvで設定しているとき
+# [え？君せっかく Python のバージョン管理に pyenv 使ってるのに Vim の補完はシステムライブラリ参照してるの？ - Λlisue's blog](http://lambdalisue.hatenablog.com/entry/2014/05/21/065845)
+py2version=$(python2 --version 2>&1 | awk '{print $2}')
+py3version=$(python3 --version 2>&1 | awk '{print $2}')
+LDFLAGS="-Wl,-rpath=${HOME}/.pyenv/versions/${py2version}/lib:${HOME}/.pyenv/versions/${py3version}/lib" ./configure \
+  --enable-fail-if-missing \
+  --enable-luainterp \
+  --enable-perlinterp \
+  --enable-pythoninterp=dynamic \
+  --enable-python3interp=dynamic \
+  --enable-rubyinterp=yes \
+  --enable-multibyte \
+  --enable-fontset \
+  --with-features=huge \
+  --disable-gui \
+  --without-x
+
+# pyenvなし
+# ./configure \
+# --with-features=huge \
+# --enable-multibyte \
+# --disable-gui \
+# --without-x \
+# --enable-rubyinterp \
+# --enable-pythoninterp \
+# --enable-perlinterp \
+# --enable-luainterp
 
 make
 sudo make install
-
-# pyenv
-sudo yum -y install bzip2-devel openssl-devel readline-devel sqlite-devel zlib-devel
 
 sudo yum -y boost-devel cmake cmake3 gcc-c++
