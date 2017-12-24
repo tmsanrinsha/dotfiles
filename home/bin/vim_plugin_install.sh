f688b08c6e9508d75daf85dae8092a2b829d08e1
tmsanrinsha <sanrinsha.tm@gmail.com>
2017-12-24 16:10:17 +0900

    vim_plugin_install.shの修正
    プロセス数を設定できるようにした
    PATHを修正
    sedのパスを修正

--- home/bin/vim_plugin_install.sh
+++ home/bin/vim_plugin_install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-PATH="/Applications/MacVim.app/Contents/MacOS:/usr/local/opt/gnu-sed/libexec/gnubin:$HOME/bin:/usr/local/bin:/usr/bin:$PATH"
+PROCESS_NUM=${1:-4}
+
+PATH="/Applications/MacVim.app/Contents/MacOS:/usr/local/opt/gnu-sed/libexec/gnubin:$HOME/bin:$PATH"
 
 if [ -x /Applications/MacVim.app/Contents/MacOS/Vim ]; then
     vim=/Applications/MacVim.app/Contents/MacOS/Vim
@@ -23,9 +25,9 @@ if [[ ! -d "$dein_repo_dir" ]]; then
 fi
 
 # yes | $vim -N -u $HOME/.vimrc -c "try | call dein#update() | finally | qall! | endtry" -U NONE -i NONE -V1 -e -s || :
-$vim -N -u $HOME/.vimrc -c "try | call dein#update() | finally | qall! | endtry" -U NONE -i NONE -V1 -e -s || :
+$vim -N -u $HOME/.vimrc -c "try | let g:dein#install_max_processes = $PROCESS_NUM | call dein#update() | finally | qall! | endtry" -U NONE -i NONE -V1 -e -s || :
 
 # プラグインのタグを加える
 ctags --tag-relative=yes -R --append=yes -f ~/src/github.com/tmsanrinsha/dotfiles/.git/vim.tags --languages=vim --vim-kinds=-m ~/.cache/vim/dein/repos
 # スクリプトローカルな関数は必要ないので削除
-sed -i'' -e '/^s:/d' ~/src/github.com/tmsanrinsha/dotfiles/.git/tags/vim.tags
+sed -i'' -e '/^s:/d' ~/src/github.com/tmsanrinsha/dotfiles/.git/vim.tags
