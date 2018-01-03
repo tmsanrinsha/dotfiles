9cdf8df8084fe8a62c917fcd4686338c351a010b
tmsanrinsha <sanrinsha.tm@gmail.com>
2018-01-03 14:07:40 +0900

    Mozc classを作った

--- home/.vim/rplugin/python3/deoplete/sources/deoplete-mozc.py
+++ home/.vim/rplugin/python3/deoplete/sources/deoplete-mozc.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 from .base import Base
 from deoplete.util import debug
+import functools
 import re
 import sys
 import subprocess
@@ -9,81 +10,92 @@ PY2 = sys.version_info < (3, 0)
 if not PY2:
     basestring = str
 
-helper = None
-mozc_debug_mode = True
-sess_count = 0
-msg_count = 0
-whitesp = " \t\r\n"
-tokens = "()"
 
+class Mozc:
 
-def parse_sexp(sexp):
-    sexp = sexp.strip(whitesp)
-    if sexp[0] == '(':
-        ret = []
-        remain = sexp[1:]
-        while remain[0] != ')':
-            e, remain = parse_sexp(remain)
-            ret.append(e)
-        if len(ret) == 3 and ret[1] == '.':
-            ret = (ret[0], ret[2])
-        elif len(ret) > 1 and isinstance(ret[0], basestring):
-            ret = (ret[0], ret[1:])
-        if all(isinstance(e, tuple) for e in ret):
-            ret = dict(ret)
-        return ret, remain[1:]
-    elif sexp[0] == '"':
-        ret = u''
-        escaped = False
-        remain = sexp[1:]
-        while escaped or remain[0] != '"':
-            if escaped:
-                escaped = False
+    def __init__(self, mozc_emacs_helper_path, debug_func=print):
+        self.__debug_func = debug_func
+        self.__debug_mode = True
+
+        # コマンドが無いときの処理
+        self.__helper = subprocess.Popen(mozc_emacs_helper_path,
+                                         stdin=subprocess.PIPE,
+                                         stdout=subprocess.PIPE)
+        responce = self.__helper.stdout.readline()
+        self.__print_debug(responce)
+        if not responce:
+            self.__print_debug("responce is empty check your 'mozc_emacs_helper' setting.")
+
+        self.__event_id = 0
+
+        self.create_session()
+
+    def create_session(self):
+        oobj = self.communicate('CreateSession')
+        self.__session_id = int(oobj["emacs-session-id"])
+
+    def delete_session(self):
+        self.communicate('DeleteSession', self.__session_id)
+
+    def sendkey(self, key):
+        oobj = self.communicate('SendKey', "{0} {1}".format(self.__session_id, key))
+        return oobj['output']
+
+    def convert(self, input):
+        pass
+
+    def communicate(self, function, arg=""):
+        fullmsg = "({0} {1} {2})\n".format(self.__event_id, function, arg)
+        self.__helper.stdin.write(bytes(fullmsg.encode("utf-8")))
+        self.__helper.stdin.flush()
+        self.__event_id += 1
+        return self.__parse_sexp(self.__helper.stdout.readline().decode('utf-8'))[0]
+
+    def __parse_sexp(self, sexp):
+        WHITE_SP = " \t\r\n"
+        TOKENS = "()"
+
+        sexp = sexp.strip(WHITE_SP)
+        if sexp[0] == '(':
+            ret = []
+            remain = sexp[1:]
+            while remain[0] != ')':
+                e, remain = self.__parse_sexp(remain)
+                ret.append(e)
+            if len(ret) == 3 and ret[1] == '.':
+                ret = (ret[0], ret[2])
+            elif len(ret) > 1 and isinstance(ret[0], basestring):
+                ret = (ret[0], ret[1:])
+            if all(isinstance(e, tuple) for e in ret):
+                ret = dict(ret)
+            return ret, remain[1:]
+        elif sexp[0] == '"':
+            ret = u''
+            escaped = False
+            remain = sexp[1:]
+            while escaped or remain[0] != '"':
+                if escaped:
+                    escaped = False
+                    ret += remain[0]
+                    remain = remain[1:]
+                elif remain[0] == '\\':
+                    escaped = True
+                    remain = remain[1:]
+                else:
+                    ret += remain[0]
+                    remain = remain[1:]
+            return ret, remain[1:]
+        else:
+            ret = ''
+            remain = sexp
+            while remain[0] not in WHITE_SP + TOKENS:
                 ret += remain[0]
                 remain = remain[1:]
-            elif remain[0] == '\\':
-                escaped = True
-                remain = remain[1:]
-            else:
-                ret += remain[0]
-                remain = remain[1:]
-        return ret, remain[1:]
-    else:
-        ret = ''
-        remain = sexp
-        while remain[0] not in whitesp+tokens:
-            ret += remain[0]
-            remain = remain[1:]
-        return ret, remain
+            return ret, remain
 
-
-def communicate(cmd, arg=""):
-    global helper, msg_count
-    fullmsg = "({0} {1} {2})\n".format(msg_count, cmd, arg)
-    helper.stdin.write(bytes(fullmsg.encode("utf-8")))
-    helper.stdin.flush()
-    msg_count += 1
-    return parse_sexp(helper.stdout.readline().decode('utf-8'))[0]
-
-
-def print_debug(*args):
-    if mozc_debug_mode:
-        # debug(self.vim, *args)
-        print(*args)
-
-
-def mozc_start():
-    global sess_count
-    # start
-    oobj = communicate('CreateSession')
-    print_debug("Start:", oobj)
-    sess_count = int(oobj["emacs-session-id"])
-
-
-def mozc_stop():
-    global sess_count
-    print_debug("End:", communicate('DeleteSession ', sess_count))
-    sess_count += 1
+    def __print_debug(self, *args):
+        if self.__debug_mode:
+            self.__debug_func(*args)
 
 
 class Source(Base):
@@ -93,34 +105,20 @@ class Source(Base):
 
         self.name = 'mozc'
         self.mark = '[Mozc]'
-        # self.filetypes = ['_']
+        # self.filetypes = ['yaml']
         self.matchers = []
         self.min_pattern_length = 1
         self.is_volatile = True
         self.rank = 500
 
-        # self.result_keywords = [{'word': x} for x in KEYWORD]
     def on_init(self, context):
         vars = context['vars']
 
-        self.foo = vars.get('deoplete#sources#travis#foo', '')
-        self.bar = vars.get('deoplete#sources#travis#bar', False)
+        mozc_emacs_helper_path = vars.get('deoplete#sources#mozc#mozc_emacs_helper_path', 'mozc_emacs_helper')
 
-        global helper
-        global mozc_debug_mode
-
-        helper = subprocess.Popen("mozc_emacs_helper",
-                                  stdin=subprocess.PIPE,
-                                  stdout=subprocess.PIPE)
-        first_responce = helper.stdout.readline()
-        print(first_responce)
-        if not first_responce:
-            print("first_responce is empty check your 'mozc_emacs_helper' setting.")
-
-        mozc_start()
-
+        self.mozc = Mozc(mozc_emacs_helper_path, functools.partial(debug, self.vim))
         try:
-            # init(load suorce) only work
+            # self.mozc = Mozc(mozc_emacs_helper_path)
             pass
         except Exception:
             # Ignore the error
@@ -137,16 +135,21 @@ class Source(Base):
 
     def get_complete_position(self, context):
         m = re.search(r'[a-zA-Z0-9_]*$', context['input'])
-        debug(self.vim, m.start())
+        debug(self.vim, 'start: ' + str(m.start()))
         return m.start() if m else -1
 
     def gather_candidates(self, context):
-        # return dict in list ([{},{},{}...])
         debug(self.vim, "input: " + context['input'])
-        oobj = communicate('SendKey', "{0} {1}".format(sess_count, context["input"][-1]))["output"]
+        # # 変換したいローマ字を送る
+        # input = context['input']
+        # candidates = self.mozc.convert(input)
+
+        # oobj = communicate('SendKey', "{0} {1}".format(sess_count, context["input"][-1]))["output"]
+        oobj = self.mozc.sendkey(context["input"][-1])
+
         # print_debug("Start:", oobj)
         # debug(self.vim, oobj)
-        debug(self.vim, oobj["all-candidate-words"])
+        debug(self.vim, oobj)
 
         candidates = [{'word': c["value"],
                        'abbr': "{0} {1}".format(c["value"], c["annotation"]["description"])}
@@ -155,6 +158,4 @@ class Source(Base):
                       for c in oobj["all-candidate-words"]["candidates"]]
 
         debug(self.vim, candidates)
-        # return self.result_keywords
         return candidates
-        # return [{'word': 'a'}, {'word': 'b', 'abbr': 'ひらがな'}]
