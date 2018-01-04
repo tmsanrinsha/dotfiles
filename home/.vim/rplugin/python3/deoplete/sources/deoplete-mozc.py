88654da8a5b825c5ca6b0d3b6ee35f7a8a808cc0
tmsanrinsha <sanrinsha.tm@gmail.com>
2018-01-04 17:37:12 +0900

    mozcを別ファイル化

--- home/.vim/rplugin/python3/deoplete/sources/deoplete-mozc.py
+++ home/.vim/rplugin/python3/deoplete/sources/deoplete-mozc.py
@@ -1,101 +1,13 @@
-from __future__ import print_function
-from .base import Base
-from deoplete.util import debug
 import functools
+import os
 import re
 import sys
-import subprocess
 
-PY2 = sys.version_info < (3, 0)
-if not PY2:
-    basestring = str
+from .base import Base
+from deoplete.util import debug
 
-
-class Mozc:
-
-    def __init__(self, mozc_emacs_helper_path, debug_func=print):
-        self.__debug_func = debug_func
-        self.__debug_mode = True
-
-        # コマンドが無いときの処理
-        self.__helper = subprocess.Popen(mozc_emacs_helper_path,
-                                         stdin=subprocess.PIPE,
-                                         stdout=subprocess.PIPE)
-        responce = self.__helper.stdout.readline()
-        self.__print_debug(responce)
-        if not responce:
-            self.__print_debug("responce is empty check your 'mozc_emacs_helper' setting.")
-
-        self.__event_id = 0
-
-        self.create_session()
-
-    def create_session(self):
-        oobj = self.communicate('CreateSession')
-        self.__session_id = int(oobj["emacs-session-id"])
-
-    def delete_session(self):
-        self.communicate('DeleteSession', self.__session_id)
-
-    def sendkey(self, key):
-        oobj = self.communicate('SendKey', "{0} {1}".format(self.__session_id, key))
-        return oobj['output']
-
-    def convert(self, input):
-        pass
-
-    def communicate(self, function, arg=""):
-        fullmsg = "({0} {1} {2})\n".format(self.__event_id, function, arg)
-        self.__helper.stdin.write(bytes(fullmsg.encode("utf-8")))
-        self.__helper.stdin.flush()
-        self.__event_id += 1
-        return self.__parse_sexp(self.__helper.stdout.readline().decode('utf-8'))[0]
-
-    def __parse_sexp(self, sexp):
-        WHITE_SP = " \t\r\n"
-        TOKENS = "()"
-
-        sexp = sexp.strip(WHITE_SP)
-        if sexp[0] == '(':
-            ret = []
-            remain = sexp[1:]
-            while remain[0] != ')':
-                e, remain = self.__parse_sexp(remain)
-                ret.append(e)
-            if len(ret) == 3 and ret[1] == '.':
-                ret = (ret[0], ret[2])
-            elif len(ret) > 1 and isinstance(ret[0], basestring):
-                ret = (ret[0], ret[1:])
-            if all(isinstance(e, tuple) for e in ret):
-                ret = dict(ret)
-            return ret, remain[1:]
-        elif sexp[0] == '"':
-            ret = u''
-            escaped = False
-            remain = sexp[1:]
-            while escaped or remain[0] != '"':
-                if escaped:
-                    escaped = False
-                    ret += remain[0]
-                    remain = remain[1:]
-                elif remain[0] == '\\':
-                    escaped = True
-                    remain = remain[1:]
-                else:
-                    ret += remain[0]
-                    remain = remain[1:]
-            return ret, remain[1:]
-        else:
-            ret = ''
-            remain = sexp
-            while remain[0] not in WHITE_SP + TOKENS:
-                ret += remain[0]
-                remain = remain[1:]
-            return ret, remain
-
-    def __print_debug(self, *args):
-        if self.__debug_mode:
-            self.__debug_func(*args)
+sys.path.insert(0, os.path.dirname(__file__))
+import mozc
 
 
 class Source(Base):
@@ -105,7 +17,7 @@ class Source(Base):
 
         self.name = 'mozc'
         self.mark = '[Mozc]'
-        # self.filetypes = ['yaml']
+        self.filetypes = ['hoge']
         self.matchers = []
         self.min_pattern_length = 1
         self.is_volatile = True
@@ -116,7 +28,7 @@ class Source(Base):
 
         mozc_emacs_helper_path = vars.get('deoplete#sources#mozc#mozc_emacs_helper_path', 'mozc_emacs_helper')
 
-        self.mozc = Mozc(mozc_emacs_helper_path, functools.partial(debug, self.vim))
+        self.mozc = mozc.Mozc(mozc_emacs_helper_path, functools.partial(debug, self.vim))
         try:
             # self.mozc = Mozc(mozc_emacs_helper_path)
             pass
@@ -144,8 +56,7 @@ class Source(Base):
         # input = context['input']
         # candidates = self.mozc.convert(input)
 
-        # oobj = communicate('SendKey', "{0} {1}".format(sess_count, context["input"][-1]))["output"]
-        oobj = self.mozc.sendkey(context["input"][-1])
+        oobj = self.mozc.send_key(context["input"][-1])
 
         # print_debug("Start:", oobj)
         # debug(self.vim, oobj)
@@ -155,7 +66,7 @@ class Source(Base):
                        'abbr': "{0} {1}".format(c["value"], c["annotation"]["description"])}
                       if "annotation" in c and "description" in c["annotation"]
                       else {'word':  c["value"]}
-                      for c in oobj["all-candidate-words"]["candidates"]]
+                      for c in oobj['output']["all-candidate-words"]["candidates"]]
 
         debug(self.vim, candidates)
         return candidates
