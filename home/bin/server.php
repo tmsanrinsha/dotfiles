251fc8ef6f432d6c7dd8356d7970d6702da9a6c1
tmsanrinsha <sanrinsha.tm@gmail.com>
2018-03-01 00:31:12 +0900

    server.phpの修正

--- home/bin/server.php
+++ home/bin/server.php
@@ -2,8 +2,8 @@
 php -S 0.0.0.0:8081 "$0"
 exit
 <?php
-$body = json_encode(file_get_contents('php:/input'), true);
+$body = json_decode(file_get_contents('php://input'), true);
 
-error_log(var_export(get_headers(), true));
+error_log(var_export(apache_request_headers(), true));
 error_log(var_export($body, true));
 error_log("\n\n\n\n\n");
