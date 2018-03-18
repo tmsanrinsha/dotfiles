0f1fe1dd1a3d2e63250125886a43eb6a61cce966
tmsanrinsha <sanrinsha.tm@gmail.com>
2018-03-18 12:35:02 +0900

    decompres.shにrpmを追加

--- home/bin/decompres.sh
+++ home/bin/decompres.sh
@@ -7,14 +7,7 @@ usage() {
     exit
 }
 
-[ "$1" = '-h' -o "$1" = '-help' -o $# -ne 1 ] && usage
-
-# # 変数展開して一番後ろの拡張子の取得
-# EXTENSION="${1##*.}"
-# # 後ろから2番目の拡張子がtarのときはtarを追加する
-# if echo $1 | grep "\.tar\.[^.]*$" 1>/dev/null 2>&1; then
-#     EXTENSION=tar.$EXTENSION
-# fi
+[ "$1" = '-h' -o "$1" = '--help' -o $# -ne 1 ] && usage
 
 # 解凍方法は
 # http://tukaikta.blog135.fc2.com/blog-entry-223.html
@@ -22,28 +15,38 @@ usage() {
 # に従った
 case $1 in
     *.tar | *.tgz | *.tar.gz | *.tar.bz2 | *.tbz | *.tbz2 | *.tar.xz | *.tar.lzma | *.tlz | *.tar.Z)
-        tar xvf $1;;
+        tar xvf "$1";;
     # 古いtarの場合
     *.tar)
-        tar xvf $1;;
+        tar xvf "$1";;
     *.tgz | *tar.gz)
-        gzip -dc $1 | tar xvf -;;
+        gzip -dc "$1" | tar xvf -;;
     *.tar.bz2 | *tbz | *tbz2)
-        bzip2 -dc $1 | tar xvf -;;
+        bzip2 -dc "$1" | tar xvf -;;
     *.tar.Z)
-        uncompress -c $1 | tar xvf -;;
+        uncompress -c "$1" | tar xvf -;;
     *.lzh)
-        lha e $1;;
+        lha e "$1";;
     *.zip)
-        unzip $1;;
+        unzip "$1";;
     *.bz2)
-        bzip2 -dc $1;;
+        bzip2 -dc "$1";;
     *.gz)
-        gunzip $1;;
+        gunzip "$1";;
     *.Z)
-        uncompress $1;;
+        uncompress "$1";;
     *.arj)
-        unarj $1;;
+        unarj "$1";;
+    *.rpm)
+        if ! which rpm2cpio.pl 1>/dev/null 2>&1; then
+            cat <<EOS
+rpm2cpio.pl not found. Please install
+for Mac:
+    brew install rpm2cpio
+EOS
+            exit 1
+        fi
+        rpm2cpio.pl "$1" | cpio -id;;
     *)
         echo 'unknown extension';
         exit 1;;
